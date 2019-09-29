dd139807cf1a33f39766fca76835a2e83e42d196
ayyoub <ayyoub@hotmail.com>
2019-09-29 17:40:07 +0200

    Updates

---
+++ precache-manifest.1f048d961e1e01b97b21719afdf87cf1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe5cc19e69d042e644f582113461c9aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af813175d975d682bc78",
+    "url": "/static/css/main.aeb64c80.chunk.css"
+  },
+  {
+    "revision": "7336c3ceeec551d451ed",
+    "url": "/static/js/2.c328e90f.chunk.js"
+  },
+  {
+    "revision": "af813175d975d682bc78",
+    "url": "/static/js/main.d0e723f7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "91076b54528636d899d0867519300dda",
+    "url": "/static/media/aviskr-thumbnail.91076b54.jpg"
+  },
+  {
+    "revision": "a3d6da85fb065161cfcf815e8320e992",
+    "url": "/static/media/contactkeeper-thumbnail.a3d6da85.JPG"
+  },
+  {
+    "revision": "6ec779f7357d79e9c63a95963612f028",
+    "url": "/static/media/uidesign-thumbnail.6ec779f7.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "ayyoubmaknassa.com/precache-manifest.b0b1f4ec8d2a991a3f449bdd22e1d2d6.js"
+  "/precache-manifest.1f048d961e1e01b97b21719afdf87cf1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("ayyoubmaknassa.com/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
